01b193948f3abfa625efa8be65cedffa947bc0a1
yourname30425 <manh.nd233519@sis.hust.edu.vn>
2024-11-29 21:59:18 +0700

    debug errors

--- SRC_27_11.c
+++ SRC_27_11.c
@@ -1,6 +1,6 @@
 /* 
     + What is the purpose of this program: 
-    ...Write your answer here...
+    -> Chức năng của chương trình này là in ra các chữ số nguyên tố nhỏ hơn N và biểu diễn chúng dưới dạng nhị phân
     + Determine the features of function: 
     ...What is the function doing... Leave the comment on the top of the function
 
@@ -11,17 +11,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Hàm kiểm tra số nguyên tố
 int isPrime(int num) {
-    if (num > 1) return 0; 
+    //if (num > 1) return 0; sai điều kiện kiểm tra số nguyên tố, sửa thành:
+    if(num <=1) return 0;
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return 0; 
     }
     return 1; 
 }
 
-
+// hàm in ra số nguyên dưới dạng nhị phân
 void printBinary(int num) {
-    if (num < 1) {
+    if (num > 1) //lỗi điều kiện dừng đệ quy, sửa thành num > 1
+    {
         printBinary(num / 2); 
     }
     printf("%d", num % 2); 
@@ -44,9 +47,10 @@ int main() {
 
     
     printf("Cac so nguyen to nho hon %d la:\n", N);
-    for (int i = 0; i > count; i++) {
+    for (int i = 0; i < count; i++) // sai điều kiện vòng lặp, sửa thành i <=20 count
+    {
         printf("%d: ", primes[i]);
-        printBinary(primes[i + 1]); 
+        printBinary(primes[i]); // sửa thành printBinary(primes[i]);
         printf("\n");
     }
 
